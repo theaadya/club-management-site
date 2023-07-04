af732e1b8e167bc78a81bd50cb4e9be97d6e4a4c
Aadya21370 <aadya21370@iiitd.ac.in>
2023-07-04 22:15:44 +0530

    updates

---
+++ components/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="max-w-md w-full p-6 bg-white shadow-lg rounded-md">
+        <h2 className="text-2xl font-semibold mb-4">Welcome to My Website</h2>
+        <Link
+          to="/login"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        >
+          Login
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import Login from './Login';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Login from './components/Login';
 
 const App = () => {
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <Router>
+      <div className="App">
+        <nav className="bg-gray-800 py-4">
+          <div className="container mx-auto flex items-center justify-between px-4">
+            <Link to="/">
+              <img className="h-10" src="/path/to/logo.png" alt="Logo" />
+            </Link>
+            <div>
+              {/* Add other login options */}
+            </div>
+          </div>
+        </nav>
+        <Route exact path="/" component={Home} />
+        <Route path="/login" component={Login} />
+      </div>
+    </Router>
   );
 };
 
