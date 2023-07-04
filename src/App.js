c2390387e21bcb8dde0cded1ef35c6806e8f36fa
Aadya21370 <aadya21370@iiitd.ac.in>
2023-07-04 23:39:53 +0530

    homepage made (almost)

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React from 'react';
 import { BrowserRouter as Router, Link, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Login from './components/Login';
+import logo from './assets/logo.png';
 
 const App = () => {
   return (
     <Router>
       <div className="App">
-        <nav className="bg-[#333333] py-7">
-          {/* ... */}
+        <nav className="bg-[#ffffff] py-5 border-b-2 border-[#666666] shadow">
+          <div className="container mx-auto flex items-center justify-between">
+            <Link to="/">
+              <img src={logo} alt="Logo" className="h-12 mr-7" />
+            </Link>
+            {/* Navigation links */}
+          </div>
         </nav>
         <Routes>
           <Route path="/" element={<Home />} />
