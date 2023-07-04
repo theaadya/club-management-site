8c36fdd26327d314ac341d9b53c916826082cfc4
Aadya21370 <aadya21370@iiitd.ac.in>
2023-07-04 22:23:25 +0530

    moree updatess

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Link, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Login from './components/Login';
 
@@ -8,17 +8,12 @@ const App = () => {
     <Router>
       <div className="App">
         <nav className="bg-gray-800 py-4">
-          <div className="container mx-auto flex items-center justify-between px-4">
-            <Link to="/">
-              <img className="h-10" src="/path/to/logo.png" alt="Logo" />
-            </Link>
-            <div>
-              {/* Add other login options */}
-            </div>
-          </div>
+          {/* ... */}
         </nav>
-        <Route exact path="/" component={Home} />
-        <Route path="/login" component={Login} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
       </div>
     </Router>
   );
