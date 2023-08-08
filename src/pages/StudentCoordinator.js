dc54a5f39ed38b6a74243994076b7b4bbf5df5c3
Aadya21370 <aadya21370@iiitd.ac.in>
2023-08-09 01:50:29 +0530

    added event info in pending events

--- src/pages/StudentCoordinator.js
+++ src/pages/StudentCoordinator.js
@@ -3,6 +3,18 @@ import Navbar from '../components/common/navbar.js';
 import axios from 'axios';
 
 const StudentCoordinator = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedEvent, setSelectedEvent] = useState(null);
+
+  const openModal = (event) => {
+    setSelectedEvent(event);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   const [events, setEvents] = useState([]);
 
   useEffect(() => {
@@ -16,6 +28,8 @@ const StudentCoordinator = () => {
     }
     fetchEvents();
   }, []);
+
+  
 
   return (
     <>
@@ -89,7 +103,7 @@ const StudentCoordinator = () => {
                 key={event._id}  // Assuming _id is the unique identifier
                 className="flex justify-between items-center py-1"
               >
-                <span>{event.name}</span>
+                <h3 onClick={() => openModal(event)}>{event.name}</h3>
                 <div>
                   <button
                     className="bg-green-500 hover:bg-green-600 text-white font-bold py-1 px-2 rounded mr-2"
@@ -138,6 +152,67 @@ const StudentCoordinator = () => {
             ))}
         </ul>
       </div>
+
+      {isModalOpen && selectedEvent && (
+          <div className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center bg-gray-800 bg-opacity-50">
+            <div className="max-w-2xl w-3/4 max-h-2xl h-3/4 p-6 bg-white shadow-lg rounded-md overflow-y-auto">
+              <h2 className="text-2xl text-center font-bold mb-4">{selectedEvent.name}</h2>
+              <div className="mb-4">
+                <label className="block font-bold">Description</label>
+                <p>{selectedEvent.description}</p>
+              </div>
+              <div className="mb-4">
+                <label className="block font-bold">Domain</label>
+                <p>{selectedEvent.domain}</p>
+              </div>
+              <div className="mb-4">
+                <label className="block font-bold">Start Date</label>
+                <p>{selectedEvent.start}</p>
+              </div>
+              <div className="mb-4">
+                <label className="block font-bold">End Date</label>
+                <p>{selectedEvent.end}</p>
+              </div>
+              <div className="mb-4">
+                <label className="block font-bold">Venue</label>
+                <p>{selectedEvent.venue}</p>
+              </div>
+              <div className="mb-4">
+                <label className="block font-bold">Coordinator</label>
+                <p>{selectedEvent.coordinator}</p>
+              </div>
+              <div className="mb-4">
+                <label className="block font-bold">Registration Deadline</label>
+                <p>{selectedEvent.registrationDeadline}</p>
+              </div>
+              <div className="mb-4">
+                <label className="block font-bold">Club</label>
+                <p>{selectedEvent.club}</p>
+              </div>
+              <div className="mb-4">
+                <label className="block font-bold">Participants</label>
+                {selectedEvent.participants.map((participant, index) => (
+                  <p key={index} className="mb-1">
+                    {participant}
+                  </p>
+                ))}
+              </div>
+              <div className="mb-4">
+                <label className="block font-bold">Creation Date</label>
+                <p>{selectedEvent.creationDate}</p>
+              </div>
+              <div className="flex justify-center">
+                <button
+                  type="button"
+                  className="ml-4 bg-gray-400 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded"
+                  onClick={closeModal}
+                >
+                  Close
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
     </>
   );
 };
