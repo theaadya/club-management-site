45819b6008ce3a4759d7fed7e8ee789eeab9f838
Aadya21370 <aadya21370@iiitd.ac.in>
2023-08-13 19:21:01 +0530

    added update club details

--- src/components/admin/LoginDetails.js
+++ src/components/admin/LoginDetails.js
@@ -46,6 +46,17 @@ const LoginDetails = () => {
     // Handle login details change logic here
   };
 
+  const updatedClubDetails = async (club) => {
+    try {
+      const updatedClub = { ...club };
+      delete updatedClub._id; // Remove the _id field from the update payload
+      await axios.put(`/clubs/${club._id}`, updatedClub);
+      closeModal();
+    } catch (error) {
+      console.error('Error updating club details:', error);
+    }
+  };
+
   return (
     <div className='py-5'>
       <h2 className="text-xl font-bold mb-4">Club Details</h2>
@@ -115,44 +126,108 @@ const LoginDetails = () => {
           <div className="max-w-2xl w-3/4 max-h-2xl h-3/4 p-6 bg-white shadow-lg rounded-md overflow-y-auto">
           <h2 className="text-2xl text-center font-bold mb-4">{selectedClub.name}</h2>
             <div className="mb-4">
+              <label className="block font-bold">Name</label>
+              <input
+                type="text"
+                className="border p-2 w-full"
+                value={selectedClub.name}
+                onChange={(e) => {
+                  const updatedClub = { ...selectedClub, name: e.target.value };
+                  setSelectedClub(updatedClub);
+                }}
+              />
+            </div>
+            <div className="mb-4">
               <label className="block font-bold">Description</label>
-              <p>{selectedClub.description}</p>
+              <textarea
+                className="border p-2 w-full"
+                value={selectedClub.description}
+                onChange={(e) => {
+                  const updatedClub = { ...selectedClub, description: e.target.value };
+                  setSelectedClub(updatedClub);
+                }}
+              />
             </div>
             <div className="mb-4">
               <label className="block font-bold">Coordinator</label>
-              <p>{selectedClub.coordinator}</p>
+              <input
+                type="text"
+                className="border p-2 w-full"
+                value={selectedClub.coordinator}
+                onChange={(e) => {
+                  const updatedClub = { ...selectedClub, coordinator: e.target.value };
+                  setSelectedClub(updatedClub);
+                }}
+              />
             </div>
             <div className="mb-4">
               <label className="block font-bold">Events</label>
-              {selectedClub.events.map((event, index) => (
-                <p key={index} className="mb-1">
-                  {event}
-                </p>
-              ))}
+              <input
+                type="text"
+                className="border p-2 w-full"
+                value={selectedClub.events.join(',')}
+                onChange={(e) => {
+                  const eventsInput = e.target.value;
+                  const updatedClub = { ...selectedClub, events: eventsInput.split(',') };
+                  setSelectedClub(updatedClub);
+                }}
+              />
             </div>
             <div className="mb-4">
               <label className="block font-bold">Email</label>
-              <p>{selectedClub.email}</p>
+              <input
+                type="text"
+                className="border p-2 w-full"
+                value={selectedClub.email}
+                onChange={(e) => {
+                  const updatedClub = { ...selectedClub, email: e.target.value };
+                  setSelectedClub(updatedClub);
+                }}
+              />
             </div>
             <div className="mb-4">
               <label className="block font-bold">Members</label>
-              {selectedClub.members.map((member, index) => (
-                <p key={index} className="mb-1">
-                  {member}
-                </p>
-              ))}
+              <input
+                type="text"
+                className="border p-2 w-full"
+                value={selectedClub.members.join(',')}
+                onChange={(e) => {
+                  const membersInput = e.target.value;
+                  const updatedClub = { ...selectedClub, members: membersInput.split(',') };
+                  setSelectedClub(updatedClub);
+                }}
+              />
             </div>
             <div className="mb-4">
               <label className="block font-bold">Creation Date</label>
-              <p>{selectedClub.creationDate}</p>
+              <input
+                type="text"
+                className="border p-2 w-full"
+                value={selectedClub.creationDate}
+                onChange={(e) => {
+                  const updatedClub = { ...selectedClub, creationDate: e.target.value };
+                  setSelectedClub(updatedClub);
+                }}
+              />
             </div>
             <div className="flex justify-center">
               <button
                 type="button"
-                className="ml-4 bg-gray-400 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded"
-                onClick={closeModal}
+                className="ml-4 bg-[#3FADA8] hover:bg-gray-400 text-white py-2 px-4 rounded"
+                onClick={() => {
+                  updatedClubDetails(selectedClub);
+                }}
               >
-                Close
+                Save Changes
+              </button>
+              <button
+                type="button"
+                className="ml-4 bg-[#3FADA8] hover:bg-gray-400 text-white py-2 px-4 rounded"
+                onClick={() => {
+                  closeModal();
+                }}
+              >
+                Cancel
               </button>
             </div>
           </div>
@@ -160,37 +235,46 @@ const LoginDetails = () => {
       )}
 
       {isLoginOpen && selectedClub && (
-      <div className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center bg-gray-800 bg-opacity-50">
-        <div className="max-w-2xl w-1/2 max-h-2xl h-1/2 py-2 bg-white shadow-lg rounded-md overflow-y-auto">
-        <h2 className="text-center text-xl font-bold mb-2 mt-10">Login Details</h2>
-        <form onSubmit={handleSubmit} className="items-center flex flex-col ">
-          <label htmlFor="clubCouncilEmail">Club Coordinator Email:</label>
-          <input
-            type="text"
-            id="clubCouncilEmail"
-            name="clubCouncilEmail"
-            className="border p-2 mb-2"
-            value={clubCouncilCoordinator}
-            onChange={(e) => setClubCouncilCoordinator(e.target.value)}
-          />
+        <div className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center bg-gray-800 bg-opacity-50">
+          <div className="max-w-2xl w-1/2 max-h-2xl h-1/2 py-2 bg-white shadow-lg rounded-md overflow-y-auto">
+            <h2 className="text-center text-xl font-bold mb-2 mt-10">Login Details</h2>
+            <form onSubmit={handleSubmit} className="items-center flex flex-col ">
+              <label htmlFor="clubCouncilEmail">Club Coordinator Email:</label>
+              <input
+                type="text"
+                id="clubCouncilEmail"
+                name="clubCouncilEmail"
+                className="border p-2 mb-2"
+                value={clubCouncilCoordinator}
+                onChange={(e) => setClubCouncilCoordinator(e.target.value)}
+              />
 
-          <label htmlFor="clubCouncilPassword">New Password:</label>
-          <input
-            type="text"
-            id="clubCouncilPassword"
-            name="clubCouncilPassword"
-            className="border p-2 mb-2"
-            value={clubCouncilCoordinator}
-            onChange={(e) => setClubCouncilCoordinator(e.target.value)}
-          />
+              <label htmlFor="clubCouncilPassword">New Password:</label>
+              <input
+                type="text"
+                id="clubCouncilPassword"
+                name="clubCouncilPassword"
+                className="border p-2 mb-2"
+                value={clubCouncilCoordinator}
+                onChange={(e) => setClubCouncilCoordinator(e.target.value)}
+              />
 
-          <button type="submit" className="bg-[#3FADA8] text-white px-4 py-2 rounded"
-            onClick={closeLogin}  
-          >
-            Save Changes
-          </button>
-        </form>
-        </div>
+              <div className="flex justify-between">  {/* Use a flex container for the buttons */}
+                <button type="submit" className="mx-4 bg-[#3FADA8] hover:bg-gray-400 text-white px-4 py-2 rounded">
+                  Save Changes
+                </button>
+                <button
+                  type="button"
+                  className="mx-4 bg-[#3FADA8] hover:bg-gray-400 text-white py-2 px-4 rounded"
+                  onClick={() => {
+                    closeLogin();
+                  }}
+                >
+                  Cancel
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       )}
     </div>
