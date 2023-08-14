96caab08e9e241ed1b3c20cb4a6b0fb9f7ce36ed
Aadya21370 <aadya21370@iiitd.ac.in>
2023-08-14 12:13:04 +0530

    added google login

--- src/components/auth/google.js
+++ src/components/auth/google.js
@@ -2,20 +2,33 @@ import React from 'react';
 import { GoogleLogin } from '@react-oauth/google';
 
 const GoogleLoginComponent = () => {
-  const handleLoginSuccess = (credentialResponse) => {
-    console.log(credentialResponse);
-    window.location.href = '/mainpage';
+  const handleLoginSuccess = async (credentialResponse) => {
+    const idToken = credentialResponse.credential;
+    const email = await fetchEmailFromGoogle(idToken);
+
+    if (email === 'aadya21370@iiitd.ac.in') {
+      console.log(credentialResponse);
+      window.location.href = '/mainpage';
+    } else {
+      console.log('Login Failed: Invalid email address');
+    }
   };
 
   const handleLoginError = () => {
     console.log('Login Failed');
   };
 
+  const fetchEmailFromGoogle = async (idToken) => {
+    const response = await fetch('https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=' + idToken);
+    const data = await response.json();
+    return data.email;
+  };
+
   return (
-    React.createElement(GoogleLogin, {
-      onSuccess: handleLoginSuccess,
-      onError: handleLoginError
-    })
+    <GoogleLogin
+      onSuccess={handleLoginSuccess}
+      onError={handleLoginError}
+    />
   );
 };
 
