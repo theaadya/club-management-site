c8f88dcc1dd83e860061e49241d908ecc4cb3ec1
Aadya21370 <aadya21370@iiitd.ac.in>
2023-08-27 11:49:52 +0530

    user info & ProfileBadge

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,112 +1,93 @@
 import React, { useState, useEffect } from 'react';
 import Navbar from '../components/common/navbar';
+import ProfileBadge from './ProfileBadge';
 import axios from 'axios';
 
-const ProfileBadge = ({ level }) => {
-  let badgeClass = '';
-
-  switch (level) {
-    case 'Student':
-      badgeClass = 'bg-blue-500';
-      break;
-    case 'Club Coordinator':
-      badgeClass = 'bg-green-500';
-      break;
-    case 'Student Club Coordinator':
-      badgeClass = 'bg-yellow-500';
-      break;
-    case 'Admin':
-      badgeClass = 'bg-red-500';
-      break;
-    default:
-      badgeClass = 'bg-gray-500';
-  }
-
-  return (
-    <span className={`inline-block px-2 py-1 text-white rounded ${badgeClass}`}>
-      {level}
-    </span>
-  );
-};
-
 const Profile = () => {
-  const [registeredEvents, setRegisteredEvents] = useState([]);
-
-  const logout = async () => {
-    try {
-      await axios.delete('/auth/api/session', {
-        withCredentials: true,
-      });
-    } catch (error) {
-      console.error('Error during logout:', error);
-    }
-  };
-
-  const [sessionData, setSessionData] = useState(null);
-  useEffect(() => {
-    async function getSessionData() {
+    const [registeredEvents, setRegisteredEvents] = useState([]);
+    const [sessionData, setSessionData] = useState(null);
+    const rollNumber = sessionData?.email ? '20' + sessionData.email.slice(-17, -12) : '';
+  
+    useEffect(() => {
+      async function getSessionData() {
+        try {
+          const response = await axios.get('/auth/api/session', {
+            withCredentials: true,
+          });
+          setSessionData(response.data);
+        } catch (error) {
+          console.error('Error fetching session data:', error);
+          setSessionData(error.message);
+        }
+      }
+  
+      getSessionData();
+    }, []);
+  
+    useEffect(() => {
+      const fetchAllEvents = async () => {
+        try {
+          const response = await axios.get('/events');
+          const allEvents = response.data.events;
+  
+          const userRegisteredEvents = allEvents.filter((event) =>
+            event.participants.some((participant) => participant.email === sessionData?.email)
+          );
+  
+          setRegisteredEvents(userRegisteredEvents);
+        } catch (error) {
+          console.error('Error fetching events:', error);
+        }
+      };
+  
+      fetchAllEvents();
+    }, [sessionData]);
+  
+    const logout = async () => {
       try {
-        const response = await axios.get('/auth/api/session', {
+        await axios.delete('/auth/api/session', {
           withCredentials: true,
         });
-        setSessionData(response.data);
       } catch (error) {
-        console.error('Error fetching session data:', error);
-        setSessionData(error.message);
-      }
-    }
-
-    getSessionData();
-  }, []);
-
-  useEffect(() => {
-    const fetchAllEvents = async () => {
-      try {
-        const response = await axios.get('/events');
-        const allEvents = response.data.events;
-
-        const userRegisteredEvents = allEvents.filter((event) =>
-          event.participants.some((participant) => participant.email === sessionData.email)
-        );
-
-        setRegisteredEvents(userRegisteredEvents);
-      } catch (error) {
-        console.error('Error fetching events:', error);
+        console.error('Error during logout:', error);
       }
     };
-
-    fetchAllEvents();
-  });
-
-  const navigationButtons = [
-    { to: '/login', label: 'Logout', onClick: logout },
-  ];
-
-  return (
-    <>
-      <Navbar navigationButtons={navigationButtons} />
-      <div className="container mx-auto mt-8">
-        <h1 className="text-3xl font-bold mb-4 mx-2">User Profile</h1>
-        <div className="bg-white p-4 rounded shadow-lg">
-          {/* <p>Name: {sessionData.name}</p> */}
-          {/* <p>Roll Number: {userData.rollNumber}</p> */}
-          <p>Email ID: {sessionData.email}</p>
-          <p>
-            Profile Type: <ProfileBadge level={sessionData.level} />
-          </p>
+  
+    const navigationButtons = [
+      { to: '/login', label: 'Logout', onClick: {logout} },
+    ];
+  
+    return (
+      <>
+        <Navbar navigationButtons={navigationButtons} />
+        <div className="container mx-auto mt-8">
+          <h1 className="text-3xl font-bold mb-4 mx-2">User Profile</h1>
+          <div className="bg-white p-4 rounded shadow-lg">
+            {sessionData ? (
+                <>
+                <p><span className="font-bold">Name:</span> {sessionData.name}</p>
+                <p><span className="font-bold">Roll Number:</span> {rollNumber}</p>
+                <p><span className="font-bold">Email ID:</span> {sessionData.email}</p>
+                <p>
+                    <span className="font-bold">Profile Type:</span> <ProfileBadge email={sessionData.email} />
+                </p>
+                </>
+            ) : (
+                <p>Loading user data...</p>
+            )}
+            </div>
+  
+          <div className="mt-4">
+            <h2 className="text-xl font-semibold mb-2 mx-2">Registered Events</h2>
+            <ul>
+              {registeredEvents.map((event) => (
+                <li key={event.id}>{event.name}</li>
+              ))}
+            </ul>
+          </div>
         </div>
-
-        <div className="mt-4">
-          <h2 className="text-xl font-semibold mb-2 mx-2">Registered Events</h2>
-          <ul>
-            {registeredEvents.map((event) => (
-              <li key={event.id}>{event.name}</li>
-            ))}
-          </ul>
-        </div>
-      </div>
-    </>
-  );
-};
-
-export default Profile;
+      </>
+    );
+  };
+  
+  export default Profile;  
---
+++ src/pages/ProfileBadge.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import axios from 'axios';
+
+const fetchUserLevel = async (email) => {
+  try {
+    const response = await axios.get('/user');
+    const users = response.data;
+    const matchingUser = users.find((user) => user.email === email);
+    if (matchingUser) {
+      return matchingUser.level;
+    } else {
+      return null;
+    }
+  } catch (error) {
+    console.error('Error fetching user level:', error);
+    return null;
+  }
+};
+
+const ProfileBadge = ({ email }) => {
+  const [level, setLevel] = React.useState(null);
+
+  React.useEffect(() => {
+    fetchUserLevel(email).then((result) => {
+      setLevel(result);
+    });
+  }, [email]);
+
+  let badgeClass = '';
+
+  switch (level) {
+    case 'Student':
+      badgeClass = 'bg-blue-500';
+      break;
+    case 'Club Coordinator':
+      badgeClass = 'bg-green-500';
+      break;
+    case 'Student Club Coordinator':
+      badgeClass = 'bg-yellow-500';
+      break;
+    case 'Admin':
+      badgeClass = 'bg-red-500';
+      break;
+    default:
+      badgeClass = 'bg-gray-500';
+  }
+
+  return (
+    <span className={`inline-block px-2 py-1 text-white rounded ${badgeClass}`}>
+      {level}
+    </span>
+  );
+};
+
+export default ProfileBadge;
